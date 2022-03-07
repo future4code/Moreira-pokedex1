a66cf5abfc5a8b2c07a4ff28b1bea16b09807692
ninazorzanello <carina.zorzanello@gmail.com>
2022-03-07 19:21:30 -0300

    criando rotas e paginas

---
+++ pokedex/src/components/HeaderHome/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+const HeaderHome = () => {
+    return (
+        <div>
+           
+           <Link to="/pokedex">Lista Pokedex</Link>
+            
+        </div>
+    )
+}
+
+export default HeaderHome
