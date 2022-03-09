83e55ca762f51ab27ec09a1da70e5a251d69a397
Atauan Lima <atauan198@gmail.com>
2022-03-09 18:29:25 -0300

    Renderização de Imagens

--- pokedex/src/pages/Home/index.jsx
+++ pokedex/src/pages/Home/index.jsx
@@ -1,31 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
 import HeaderHome from "../../components/HeaderHome";
 import useRequestData from '../../hooks/useRequestData';
 import {BASE_URL} from '../../constants/BASE_URL';
+import GlobalStateContext from "../../Global/GlobalStateContext";
 
 export default function Home(){
 
-  const pokemon = useRequestData([{}], `${BASE_URL}/pokemon`);
-    console.log(pokemon)
+const pokemons =useContext(GlobalStateContext)
   
-    
-    const listaPokemon = pokemon.map((item) =>{
-    return(
-      <div key={item.id}> 
-      <p>{item.name}</p>
-      <img src={item.url}/>
-      
-      </div>
-    );
-  })
 
     return (
         <div>
             <h1>Página Home</h1>
             <HeaderHome />
             <Link to="/detalhes">Ir para Detalhes</Link>
-            {listaPokemon}
+            {pokemons.map (list =>(
+             <> 
+             <p>{list.name}</p>
+             <img src={list.image} alt={list.name} style={{width: '10%'}}/>
+             </>
+            ))
+
+            
+            }
         </div>
         
     )
