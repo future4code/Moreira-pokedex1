36add1fd5cd5f3c4b190316a658c6e0d60f40f05
Carlise Debona <92445126+Carlisegd@users.noreply.github.com>
2022-03-11 12:47:01 -0300

    pagina pokedex com as funções

--- pokedex/src/components/HeaderHome/index.js
+++ pokedex/src/components/HeaderHome/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import {Div, Img, ButtonBack, Nav, ButtonCreate, Button} from './styled';
+import {BiLogIn} from 'react-icons/bi';
+
 
 
 const HeaderHome = () => {
     return (
-        <div>
-           
-           <Link to="/pokedex">Lista Pokedex</Link>
-            
-        </div>
+      <Div>
+        <Link to="/pokedex"><BiLogIn className="icon"/>Lista Pokedex</Link>
+      </Div>
     )
 }
 
